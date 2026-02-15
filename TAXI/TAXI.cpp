1706345644e5692c5626a068680783088f29c3eb
mdomv <322932@stud.umk.pl>
2026-02-15 15:38:48 +0100

    dodano pokazywanie zarobku na godzinę. poprawiono funkcję minimum godzin

--- TAXI/TAXI.cpp
+++ TAXI/TAXI.cpp
@@ -28,6 +28,7 @@ void pokaz_wariant(const char* opis, float paliwo, float spalanie, float ilosc_k
 int minimum_godzin(float,float,float,float,float,float);
 float koszt_na_kilometr(float, float);
 float koszty_paliwa(float, float,float,float,float);
+float zysk_na_godzine(float,float);
 int main()
 {
 	float koszty_stale;
@@ -43,10 +44,10 @@ int main()
 	float corolla_spalanie = 5;
 	
 	
-	pokaz_wariant("Xsara: ", paliwo1, spalanie1, ilosc_kursow3, kwota_za_kurs3, koszty_stale, ilosc_godzin1, ilosc_godzin2, ilosc_godzin3, kurs);
+	pokaz_wariant("Xsara: ", paliwo1, spalanie1, ilosc_kursow1, kwota_za_kurs3, koszty_stale, ilosc_godzin1, ilosc_godzin2, ilosc_godzin3, kurs);
 	//pokaz_wariant("Cos pomiedzy:", paliwo2, spalanie2, ilosc_kursow2, kwota_za_kurs2, koszty_stale, ilosc_godzin1, ilosc_godzin2, ilosc_godzin3, kurs);
 	//pokaz_wariant("Najgorsza opcja:", paliwo3, spalanie3, ilosc_kursow1, kwota_za_kurs1, koszty_stale, ilosc_godzin1, ilosc_godzin2, ilosc_godzin3, kurs);
-	pokaz_wariant("Corolla: ",corolla_paliwo,corolla_spalanie,ilosc_kursow3,kwota_za_kurs3,koszty_stale,ilosc_godzin1,ilosc_godzin2, ilosc_godzin3,kurs);
+	pokaz_wariant("Corolla: ",corolla_paliwo,corolla_spalanie,ilosc_kursow1,kwota_za_kurs3,koszty_stale,ilosc_godzin1,ilosc_godzin2, ilosc_godzin3,kurs);
 	//cout << "Dupa" << endl;
 	return 0;
 }
@@ -88,7 +89,12 @@ void pokaz_wariant(const char* opis, float paliwo, float spalanie, float ilosc_k
 	koszt1 = koszty_paliwa(paliwo, spalanie, ilosc_kursow, kurs, ilosc_godzin1);
 	koszt2 = koszty_paliwa(paliwo, spalanie, ilosc_kursow, kurs, ilosc_godzin2);
 	koszt3 = koszty_paliwa(paliwo, spalanie, ilosc_kursow, kurs, ilosc_godzin3);
-	
+	float zarobek1 = 0;
+	float zarobek2 = 0;
+	float zarobek3 = 0;
+	zarobek1 = zysk_na_godzine(zysk_netto(ilosc_godzin1, ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs), ilosc_godzin1);
+	zarobek2 = zysk_na_godzine(zysk_netto(ilosc_godzin2, ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs), ilosc_godzin2);
+	zarobek3 = zysk_na_godzine(zysk_netto(ilosc_godzin3, ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs), ilosc_godzin3);
 	cout << opis << endl;
 	cout << "Cena paliwa: " << paliwo << endl;
 	cout << "Spalanie: " << spalanie << endl;
@@ -100,21 +106,25 @@ void pokaz_wariant(const char* opis, float paliwo, float spalanie, float ilosc_k
 		<< "zl 60: " << zysk_netto(ilosc_godzin2, ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs)
 		<< "zl 600: " << zysk_netto(ilosc_godzin3, ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs) << "zl" << endl;
 	cout << "Koszty paliwa: 40: " << koszt1 << "zl 60: " << koszt2
-		 << "zl 600: " << koszt3  << "zl" << endl;
+		 << "zl 600: " << koszt3 << "zl" << endl;
+	cout << "Zarobek na godzinę 40: " << zarobek1 << "zl 60: " << zarobek2 << "zl 600: " << zarobek3 << "zl" << endl;
 	
 	int godziny = 0;
 	godziny = minimum_godzin(ilosc_kursow, kwota_za_kurs, koszty_stale, paliwo, spalanie, kurs);
 	cout << "Minimalna ilosc godzin dla " << opis << godziny << endl << endl;
 }
 int minimum_godzin(float ilosc_kursow, float kwota_za_kurs, float koszty_stale, float paliwo, float spalanie, float kurs) {
+	float brutto = ilosc_kursow * kwota_za_kurs;
+	float prowizja = brutto * prowizja_bolt;
 	float zysk_na_godzine = 0;
-	zysk_na_godzine = ilosc_kursow * kwota_za_kurs - koszt_na_kilometr(paliwo, spalanie) * kurs * ilosc_kursow;
+	zysk_na_godzine = brutto - prowizja
+		- koszt_na_kilometr(paliwo, spalanie) * kurs * ilosc_kursow;
 	if (zysk_na_godzine <= 0) {
 		cout << "Zysk na godzinę jest mniejszy lub równy zero! Nie można obliczyć minimalnej liczby godzin." << endl;
 		return -1;
 	}
 	int godziny = 0;
-	int zysk = 0;
+	float zysk = 0;
 	while (zysk < koszty_stale) {
 		zysk += zysk_na_godzine;
 		godziny++;
@@ -124,4 +134,9 @@ int minimum_godzin(float ilosc_kursow, float kwota_za_kurs, float koszty_stale, 
 float koszty_paliwa(float paliwo, float spalanie, float ilosc_kursow, float kurs,float ilosc_godzin) {
 	float koszt = koszt_na_kilometr(paliwo, spalanie)*ilosc_kursow*kurs*ilosc_godzin;
 	return koszt;
+}
+float zysk_na_godzine(float zysk, float ilosc_godzin) {
+	float zarobek = 0;
+	zarobek = zysk / ilosc_godzin;
+	return zarobek;
 }
